18e73edb08c998d301879b8feb6ba9290a776a09
Vidit Dhull <vidit.dh@gmail.com>
2022-10-23 19:54:54 +0530

    23 Oct Commit 1

--- 5. Diagonal Difference.py
+++ 5. Diagonal Difference.py
@@ -2,12 +2,16 @@ arr = [[1,2,3],[4,5,6],[7,8,9]]
 
 
 def diagonalDifference(arr):
-    sumr = arr[0][0] + arr[1][1] + arr[2][2]
-    suml = arr[2][0] + arr[1][1] + arr[0][2]
+    sumr = 0
+    suml = 0
+    for i in range(n):
+        sumr = sumr + arr[i][i]
+    for i in range(n):
+        suml = suml + arr[n-i-1][i]
     if sumr > suml:
-        return sumr-suml
+        return (sumr) - (suml)
     elif suml > sumr:
-        return suml-sumr
+        return (suml)-(sumr)
     else:
         return 0
 
