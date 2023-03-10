92c446643521e0e026f17a2b45f511e52bcf3797
Vidit Dhull <vidit.dh@gmail.com>
2023-03-10 08:31:42 +0530

    Commit 2 March 10

---
+++ 16. MaximumProductSubarray.py
@@ -0,0 +1,14 @@
+class practice:
+    def Max_product_array(self, nums):
+        res = max(nums)
+        curMin, curMax = 1, 1
+
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+            tmp = curMax * n
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+            res = max(res, curMax)
+        return res
