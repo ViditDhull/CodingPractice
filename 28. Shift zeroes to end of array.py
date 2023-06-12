f2c8394046a9921e286144f957eb885d9c30e3eb
Vidit Dhull <vidit.dh@gmail.com>
2023-06-12 19:40:15 +0530

    June12 Commit 1

---
+++ 28. Shift zeroes to end of array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        for i in range(len(nums)):
+            if nums[i]==0:
+                nums.remove(nums[i])
+                nums.append(0)
+        return nums
