35d9f359c72f3dcf495f9c9fca0e6ce415843f92
Vidit Dhull <vidit.dh@gmail.com>
2023-07-15 22:27:44 +0530

    Count Substring

---
+++ 59. Count Substring.py
@@ -0,0 +1,19 @@
+def count_substring(string, sub_string):
+    count = 0
+    start = 0
+    while True:
+        index = string.find(sub_string, start)
+        
+        if index == -1:
+            break
+        
+        count += 1
+        start = index +1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
