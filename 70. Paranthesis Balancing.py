9eff38170debb60f7ee98430cf7aa84165dbe1f4
Vidit Dhull <vidit.dh@gmail.com>
2023-07-23 11:05:53 +0530

    Parantesis Balancing

---
+++ 70. Paranthesis Balancing.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+    
+    def push(self, val):
+        self.container.append(val)
+
+    def pop(self):
+        return self.container.pop()
+    
+    def peek(self):
+        return self.container[-1]
+    
+    def is_empty(self):
+        return len(self.container) == 0
+    
+    def size(self):
+        return len(self.container)
+
+def is_balanced(str_ele):
+    s = Stack()
+
+    for ch in str_ele:
+        s.push(ch)
+    
+    result = True
+    
+    if s.container.count('[') != s.container.count(']'):
+        result = False
+    if s.container.count('{') != s.container.count('}'):
+        result = False
+    if s.container.count('(') != s.container.count(')'):
+        result = False
+    return result
+
+
+print(is_balanced("({a+b})"))
+print(is_balanced("))((a+b}{"))
+print(is_balanced("((a+b))"))
+print(is_balanced("))"))
+print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
