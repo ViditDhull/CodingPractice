8fee6075d0b39aa36ab6d8ddc8c0bbb8f7b42f65
Vidit Dhull <vidit.dh@gmail.com>
2023-08-23 21:58:37 +0530

    Shortest Path

--- 92. Graph.py
+++ 92. Graph.py
@@ -27,6 +27,25 @@ class Graph:
 
         return paths
     
+    def get_shortest_path(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return path
+        
+        if start not in self.graph_dict:
+            return None
+        
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+        
+        return shortest_path
+    
 if __name__ == "__main__":
     routes = [
         ("Mumbai", "Paris"),
@@ -42,4 +61,4 @@ if __name__ == "__main__":
     start = "Mumbai"
     end = "New York"
 
-    print(f"Path between {start} and {end}: ", route_graph.get_paths(start, end))
+    print(f"Shortest Path between {start} and {end}: ", route_graph.get_shortest_path(start, end))
