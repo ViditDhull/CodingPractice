c0a1b2dac11799090ae4198ee6fd1dfdf6a3a953
Vidit Dhull <vidit.dh@gmail.com>
2023-08-26 13:04:51 +0530

    Number of Provinces

---
+++ 95. Number of Provinces.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findCircleNum(self, isConnected):
+        N = len(isConnected)
+        visited = set()
+        def dfs(cityI):
+            cityIConnections = isConnected[cityI]
+            visited.add(cityI)
+            for cityJ in range(N):
+                if (cityJ not in visited) and (cityIConnections[cityJ] == 1) and (cityI != cityJ):
+                    dfs(cityJ)
+            return
+        numProvinces = 0
+        for cityI in range(N):
+            if cityI not in visited:
+                dfs(cityI)
+                numProvinces += 1
+        return numProvinces
