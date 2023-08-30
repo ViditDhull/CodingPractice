73f7b34433683309e5aeaa02524acda2e53e0f75
Vidit Dhull <vidit.dh@gmail.com>
2023-08-30 07:49:50 +0530

    Dictionary Sort

--- 97. Bubble Sort.py
+++ 97. Bubble Sort.py
@@ -1,17 +1,21 @@
 def bubble_sort(elements):
     size = len(elements)
-    for i in range(size - 1):
+
+    for i in range(size-1):
         swapped = False
-        for j in range(size - 1): #need to go till last second element only
-            if elements[i] > elements[i+1]:
-                elements[i], elements[i+1] = elements[i+1], elements[i]
+        for j in range(size-1-i):
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
                 swapped = True
+
         if not swapped:
             break
 
 
-if __name__ == "__main__":
-    elements = [5, 9, 2, 1, 67, 34, 88, 34]
+if __name__ == '__main__':
+    elements = [5,9,2,1,67,34,88,34]
 
     bubble_sort(elements)
     print(elements)
