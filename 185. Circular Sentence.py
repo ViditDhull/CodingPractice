ae41dc9bbf3e82d5df5e92e1260c87dcf1b86628
Vidit Dhull <vidit.dh@gmail.com>
2023-12-29 12:33:24 +0530

    Delete 185. Circular Sentence.py

--- 185. Circular Sentence.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isCircularSentence(self, sentence: str) -> bool:
-        sa = sentence.split()
-        if sa[0][0] != sa[-1][-1]:
-            return False
-        for i in range(len(sa) - 1):
-            if sa[i][-1] != sa[i+1][0]:
-                return False
-        return True
