b58adb61d309f9ef41c879d6eebc4203246ef1d3
Vidit Dhull <vidit.dh@gmail.com>
2024-10-22 15:31:58 +0530

    Remove All Adjacent

---
+++ 333. Remove All Adjacent.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = ['']
+
+        for i in s:
+            if i == stack[-1]:
+                stack.pop(-1)
+            else:
+                stack.append(i)
+
+        return ''.join(stack)
